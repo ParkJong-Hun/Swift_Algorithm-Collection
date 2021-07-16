9671ee82fd4de8c6052bed9c9cd436f46cf83af2
ParkJong-Hun <thesn515@gmail.com>
2021-07-16 16:36:35 +0900

    2021/07/16 16:36

---
+++ DecimalReverseArray.playground/Contents.swift
@@ -0,0 +1,16 @@
+func solution(_ n:Int64) -> [Int] {
+    var x:Int = Int(n)
+    var a:Int = x
+    var answer:[Int] = []
+    while (x > 10) {
+        a %= 10
+        if(a < 10) {
+            answer.append(a)
+            x = x / 10
+            a = x
+        }
+    }
+    answer.append(x)
+    return answer
+}
+solution(12345)
---
+++ Watermelon.playground/Contents.swift
@@ -0,0 +1,11 @@
+func solution(_ n:Int) -> String {
+    var answer:String = ""
+    for i in 1...n {
+        if i % 2 == 0 {
+            answer += "박"
+        } else {
+            answer += "수"
+        }
+    }
+    return answer
+}
