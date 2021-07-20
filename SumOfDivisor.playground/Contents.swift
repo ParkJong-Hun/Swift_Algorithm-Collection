d17f67b8481a0172651a4466945aa901c8148667
ParkJong-Hun <thesn515@gmail.com>
2021-07-20 10:59:26 +0900

    2021/07/20 10:59

---
+++ SumOfDivisor.playground/Contents.swift
@@ -0,0 +1,14 @@
+func solution(_ n:Int) -> Int {
+    if n == 0 {
+        return 0
+    }
+    var sum = 0
+    for i in 1...n {
+        if n % i == 0 {
+            sum += i
+        }
+    }
+    return sum
+}
+solution(12)
+solution(5)
