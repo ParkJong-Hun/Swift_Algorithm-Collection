7212eb28611456afa122a8afc203cedfadfb1a82
ParkJong-Hun <thesn515@gmail.com>
2021-08-04 16:23:47 +0900

    2021/08/04 16:23

--- RightBracket.playground/Contents.swift
+++ RightBracket.playground/Contents.swift
@@ -1,12 +1,11 @@
 func solution(_ s:String) -> Bool {
-    var ans = false
     var a:[Character] = []
     for i in s {
         if i == "(" {
             a.append(i)
         } else {
             if a.isEmpty {
-                return ans
+                return false
             }
             a.popLast()
         }
