c82f5ff5e52f03aa4cf27e5db026682a73a75c9e
ParkJong-Hun <thesn515@gmail.com>
2021-08-13 09:33:09 +0900

    2021/08/13 09:33

--- FindPrimeNumber.playground/Contents.swift
+++ FindPrimeNumber.playground/Contents.swift
@@ -1,55 +1,41 @@
 import Foundation
 
 func solution(_ numbers:String) -> Int {
-    func dfs(_ number:[String], _ current:String) -> Bool {
-        var n = number
-        stack.append(Int(current)!)
-        if stack.isEmpty {
-            stack.append(Int(current)!)
-        }
-        if n.count == 0 {
-            return false
-        }
-        for _ in 0..<n.count {
-            let next = n.removeFirst()
-            dfs(n, "\(current)\(next)")
-            n.append(next)
-        }
-        return true
-    }
-    func isPrimeNumber(_ number:Int) -> Bool {
-        if number <= 1 {
-            return false
-        }
-        for i in 2..<Int(sqrt(Double(number))) {
-            if number % i == 0 {
-                return false
+    func dfs(_ cnt: Int, _ totalCnt: Int, _ list: [String.Element], _ current: String) {
+        if cnt == totalCnt {
+            isPrimeNumber(current)
+            return
+        } else {
+            for i in 0..<list.count {
+               var removedArray = list
+                let k = removedArray.remove(at: i)
+                dfs(cnt + 1, totalCnt, removedArray, "\(current)\(k)")
             }
         }
-        return true
     }
-    
-    var stack:[Int] = []
-    var answer = 0
-    var number:[String] = []
-    var current:String
-    var set:Set<Int> = []
-    for i in numbers {
-        number.append(String(i))
-    }
-    for i in 0..<number.count {
-        number.sort()
-        current = number.remove(at: i)
-        dfs(number, current)
-        number.append(current)
-    }
-    set = Set(stack)
-    for i in set {
-        if isPrimeNumber(i) {
-            answer += 1
+    func isPrimeNumber(_ number: String) {
+        var c = 0
+        let n = Int(number)!
+        if n > 1 {
+            for i in 2...n {
+                if c > 1 {
+                    break
+                }
+                if n % i == 0 {
+                    c += 1
+                }
+            }
+            if c == 1 {
+                set.insert(n)
+            }
         }
     }
-    return answer
+    var set: Set<Int> = []
+    let arr = Array(numbers)
+    for i in 1...arr.count{
+        dfs(0, i, arr, "")
+    }
+    return set.count
 }
 solution("17") //3
 solution("011") //2
