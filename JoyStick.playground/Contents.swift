faa19ebfcaba24e2e28766bb640da513e78f5a62
ParkJong-Hun <thesn515@gmail.com>
2021-08-19 08:35:22 +0900

    2021/08/19 08:35

---
+++ JoyStick.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func solution(_ name:String) -> Int {
+    let name = name.map({$0})
+    var count = 0
+    for i in 0..<name.count {
+        if name[i] != "A" {
+            let upMin = Int(name[i].asciiValue!) - Int("A".unicodeScalars.first!.value)
+            let downMin = Int("Z".unicodeScalars.first!.value) - Int(name[i].asciiValue!) + 1
+            if upMin < downMin {
+                count += upMin
+            } else {
+                count += downMin
+            }
+        }
+    }
+    var minMove = name.count-1
+    for i in 0..<name.count {
+        if name[i] != "A" {
+            var next = i + 1
+            while next < name.count && name[next] == "A" {
+                next += 1
+            }
+            let move = i * 2 + name.count - next
+            minMove = min(move, minMove)
+        }
+    }
+    count += minMove
+    return count
+}
+solution("JEROEN")  //56
+solution("JAN")     //23
